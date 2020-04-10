cbe61f5e40dee6b968bdd5fd3373622569a9f45a
Favour George <phavorsparks@gmail.com>
2020-04-10 08:02:16 +0000

    update routes

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World, Nginx and Express running on our server');
+	res.send('Welcome to Tribrx');
+});
+
+app.get('/now', (req, res) => {
+	res.set('X-powered-by', 'tribrx');
+	res.status(200);
+	res.send('What do you want to do now?');
 });
 
 app.get('/demo', (req, res) => {
 	res.set('X-full-stack', 'tribrx');
 	res.status(418);
 	res.send('I prefer tea!');
-})
+});
 
 app.listen(port, () => console.log('App listening on http://localhost:' + port));
