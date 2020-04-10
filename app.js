c388766391a861c2eee74abcccecde7d0550e4a9
Favour George <phavorsparks@gmail.com>
2020-04-10 08:04:56 +0100

    add new route demo

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
 	res.send('Hello World, Nginx and Express running on our server');
 });
 
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', 'tribrx');
+	res.status(418);
+	res.send('I prefer tea!');
+})
+
 app.listen(port, () => console.log('App listening on http://localhost:' + port));
